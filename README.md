282540e1149dff3d3885e387a71beb4d047aee11
Brian M. Schilder <34280215+bschilder@users.noreply.github.com>
2023-03-08 22:49:19 +0000

    update readme

--- README.md
+++ README.md
@@ -23,9 +23,9 @@ source(f)
 list_reports()
 ```
 
-## [atac_dnase_chip_example](https://neurogenomics.github.io/EpiArchives/atac_dnase_chip_example/EpiCompare.html)
+## [atac_dnase_chip_example](https://neurogenomics.github.io/EpiArchives/reports/atac_dnase_chip_example/EpiCompare.html)
 
-## [atac_dnase_example](https://neurogenomics.github.io/EpiArchives/atac_dnase_example/EpiCompare.html)
+## [atac_dnase_example](https://neurogenomics.github.io/EpiArchives/reports/atac_dnase_example/EpiCompare.html)
 
 <a href='https://github.com/neurogenomics/EpiArchives' target='_blank'><img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" width="100" height="100"></a>
 
--- code/list_reports.R
+++ code/list_reports.R
@@ -1,13 +1,16 @@
 list_reports <- function(dir=here::here("reports"),
                          pattern = "EpiCompare.html$",
                          domain="https://neurogenomics.github.io/EpiArchives"){
+    # devoptera::args2vars(list_reports, reassign = TRUE)
+    wd <- getwd()
     #### Find files ####    
     files <- list.files(path = dir, 
-                        pattern = pattern, 
+                        pattern = pattern,
+                        full.names = TRUE,
                         recursive = TRUE)
     #### Construct links ####
-    links <- stats::setNames(paste0(domain,"/",files),
-                             dirname(files))
+    links <- stats::setNames(gsub(wd,domain,files),
+                             basename(dirname(files)))
     #### Make markdown elements #### 
     md <- lapply(names(links), function(nm){
         paste0("## [",nm,"](",links[[nm]],")") 
