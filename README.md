ee41533f6cc1cb367622e2fb59ad98e788bbd2c5
Brian M. Schilder <34280215+bschilder@users.noreply.github.com>
2023-03-08 22:23:57 +0000

    Add first reports

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
+`EpiArchives`
+================
+Brian M. Schilder
+<h4>  
+README updated: <i>Mar-08-2023</i>  
+</h4>
+
+``` r
+f <- list.files("code",pattern = "*.R$", full.names = TRUE)
+source(f)
+```
+
 # EpiArchives
-Public archive for EpiCompare reports.
+
+Public archive for
+[`EpiCompare`](https://github.com/neurogenomics/EpiCompare) reports.
+
+# Reports
+
+``` r
+list_reports()
+```
+
+## [atac_dnase_chip_example](https://neurogenomics.github.io/atac_dnase_chip_example/EpiCompare.html)
+
+## [atac_dnase_example](https://neurogenomics.github.io/atac_dnase_example/EpiCompare.html)
+
+<hr>
---
+++ code/list_reports.R
@@ -0,0 +1,16 @@
+list_reports <- function(dir=here::here("reports"),
+                         pattern = "EpiCompare.html$",
+                         domain="https://neurogenomics.github.io"){
+    #### Find files ####    
+    files <- list.files(path = dir, 
+                        pattern = pattern, 
+                        recursive = TRUE)
+    #### Construct links ####
+    links <- stats::setNames(paste0(domain,"/",files),
+                             dirname(files))
+    #### Make markdown elements #### 
+    md <- lapply(names(links), function(nm){
+        paste0("## [",nm,"](",links[[nm]],")") 
+    })
+    cat(paste(md,collapse = "\n"))
+}
